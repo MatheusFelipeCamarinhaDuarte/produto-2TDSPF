cecd038c24c81f5b28f0e3f02a01bcec8e10cead
Matheus <matheus.duarte.lins@gmail.com>
2024-02-22 10:38:48 -0300

    Feita alterações para bancos e dias

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -4,18 +4,22 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        String dia = LocalDate.now().getDayOfWeek().equals(DayOfWeek.MONDAY) ? "fiap" : "casa";
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("fiap");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(dia);
         EntityManager manager = factory.createEntityManager();
 
         Produto produto = new Produto();
-        produto.setNome("NoteBook HP");
+        produto.setNome("NoteBook HP").setPreco(50.99);
 
         manager.getTransaction().begin();
-        manager.persist( produto );
+        manager.persist(produto);
         manager.getTransaction().commit();
 
 
--- src/main/java/br/com/fiap/Produto.java
+++ src/main/java/br/com/fiap/Produto.java
@@ -1,18 +1,22 @@
 package br.com.fiap;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table (name = "TB_2TDSPF_PROUTO")
 public class Produto {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PRODUTO")
+    @SequenceGenerator(name = "SQ_PRODUTO", sequenceName = "SQ_PRODUTO", initialValue = 1, allocationSize = 50)
+    @Column(name = "ID_PRODUTO")
     private Long id;
 
+    @Column(name = "NM_PRODUTO")
     private String nome;
+
+    @Column(name = "PRECO")
+    private double preco;
 
     public Long getId() {
         return id;
@@ -31,4 +35,24 @@ public class Produto {
         this.nome = nome;
         return this;
     }
+
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Produto setPreco(Double preco) {
+        this.preco = preco;
+        return this;
+    }
+
 }
